120f3f846822de400e1323759d45e98db72473d4
ilyassakhiyev <sahievilas53@gmail.com>
2025-02-07 18:17:26 +0500

    files3

---
+++ lab3/classes/1-exercise.py
@@ -0,0 +1,13 @@
+class MyString:
+    def __init__(self):
+        self.string = ""
+
+    def getString(self):
+        self.string = input("Enter a string: ")
+
+    def printString(self):
+        print(self.string.upper())
+
+b = MyString()
+b.getString()
+b.printString()
