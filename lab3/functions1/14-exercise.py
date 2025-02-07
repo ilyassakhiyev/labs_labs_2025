5b7a9abbb817359f96fb8010111a9adc44d84536
ilyassakhiyev <sahievilas53@gmail.com>
2025-02-07 21:27:56 +0500

    files3

---
+++ lab3/functions1/14-exercise.py
@@ -0,0 +1,7 @@
+from grams import grams
+from volumes import sphere_volume
+
+radius = int(input("Enter the radius: "))
+print(sphere_volume(radius))  
+gram = float(input("gram: "))
+print("ounces= ", grams(gram))
--- lab3/functions1/grams.py
+++ lab3/functions1/grams.py
@@ -2,5 +2,5 @@ def grams(gram):
     ounces = 0.03527396 * gram
     return ounces
 
-gram=float(input("gram:"))
-print("ounces=",grams(gram))
+gram=float(input("gram: "))
+print("ounces= ",grams(gram))
