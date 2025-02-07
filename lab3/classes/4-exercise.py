b2cf02f144de513f2a5daf08058f44e0f53014a3
ilyassakhiyev <sahievilas53@gmail.com>
2025-02-07 18:54:16 +0500

    files3

---
+++ lab3/classes/4-exercise.py
@@ -0,0 +1,23 @@
+import math
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def show(self):
+        print(f"Point coordinates: ({self.x}, {self.y})")
+
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+
+    def dist(self, other_point):
+        return math.sqrt((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2)
+
+
+point1 = Point(1, 2)
+point2 = Point(4, 6)
+point1.show()
+point2.show()
+print("Distance between points:", point1.dist(point2))
