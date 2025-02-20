d81358789bfadc32c830b8401fee730dbee55b83
ilyassakhiyev <sahievilas53@gmail.com>
2025-02-20 20:12:27 +0500

    files4

---
+++ lab4/math/2.py
@@ -0,0 +1,9 @@
+h = int(input())
+b1 = int(input())
+b2 = int(input())
+
+print("Height:", h)
+print("Base, first value:", b1)
+print("Base, second value:", b2)
+A = ((b1+b2)/2)*h
+print("Expected Output:", A)
